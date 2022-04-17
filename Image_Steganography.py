ece9c3dee679035e3b32479bb32dbc5b74c35af5
kaltrinabaliu <93614313+kaltrinabaliu@users.noreply.github.com>
2022-04-17 15:09:11 +0200

    encode

--- Image_Steganography.py
+++ Image_Steganography.py
@@ -21,3 +21,35 @@ def hidedata(img, data):
     b_data = data2binary(data)
     len_data = len(b_data)
 
+#iterate pixels from image and update pixel values
+
+    for value in img:
+        for pix in value:
+            r, g, b = data2binary(pix)
+            if d_index < len_data:
+                pix[0] = int(r[:-1] + b_data[d_index])
+                d_index += 1
+            if d_index < len_data:
+                pix[1] = int(g[:-1] + b_data[d_index])
+                d_index += 1
+            if d_index < len_data:
+                pix[2] = int(b[:-1] + b_data[d_index])
+                d_index += 1
+            if d_index >= len_data:
+                break
+    return img
+
+#encoding the image
+def encode():
+    img_name = input("\nEnter image name with extension: ")
+    image = cv2.imread(img_name)
+    img = Image.open(img_name, 'r')
+    w, h = img.size
+    data = input("\nEnter message: ")
+    if len(data) == 0:
+        raise ValueError("Empty data")
+    enc_img = input("\nEnter encoded image name: ")
+    enc_data = hidedata(image, data)
+    cv2.imwrite(enc_img, enc_data)
+    img1 = Image.open(enc_img, 'r')
+    img1 = img1.resize((w, h),Image.Resampling.LANCZOS)
